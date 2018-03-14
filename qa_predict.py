2b929ed3c8cdb38d18c6d898aca65cf5f7873a81
Leon Botros <leon.botros@gmail.com>
2018-03-14 14:39:22 +0100

    fixed prediction script to select non-trivial answer spans

--- qa_predict.py
+++ qa_predict.py
@@ -41,13 +41,7 @@ def get_train_samples(train_data):
 def get_best_answer_span(start_probs, end_probs):
     """ Finds best answer span (i, j) s.t.
      i <= j and start_probs[i] * end_probs[j] is maximum
-     NOTE: very very very inefficient solution
     """
-    sorted_spans = sorted([(i, j, k * l) for i, k in enumerate(start_probs) for j, l in enumerate(end_probs) if i <= j],
-                          key=lambda x: x[2])
-    return sorted_spans[0][0], sorted_spans[0][1]
-
-def get_best_answer_span2(start_probs, end_probs):
     max_i, max_j = 0, 1
     max_prob = start_probs[max_i] * end_probs[max_j]
 
@@ -59,8 +53,6 @@ def get_best_answer_span2(start_probs, end_probs):
             max_i = j
 
     return max_i, max_j
-
-
 
 
 def main():
@@ -97,7 +89,7 @@ def main():
 
     model.load_weights('simple_bidaf.h5', by_name=True)
 
-    print("predicting..")
+    print("Predicting..")
     ps_start, ps_end = model.predict([context_seqs_padded, question_seqs_padded])
     predictions = {}
     for idx, (id, s, e) in enumerate(zip(ids, ps_start, ps_end)):
@@ -106,7 +98,7 @@ def main():
         print(i, j)
         answer = " ".join([inv_word_index[x] for x in context_seqs_padded[idx][i:j] if x])
         print(answer)
-        predictions[idx] = answer
+        predictions[id] = answer
 
     with open('predictions.txt', 'w') as f:
         json.dump(predictions, f)
